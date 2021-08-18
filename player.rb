eab3381b5cac15c951a7e8d806638f95a20215e5
Miguel Jimenez <migang9@gmail.com>
2021-08-17 23:24:50 -0400

    class player and var def

---
+++ player.rb
@@ -0,0 +1,21 @@
+# class player and name
+class Player
+  attr_reader :name
+
+  #number of lives def
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+
+  #live countdown
+  def lose_life
+    @lives -= 1
+  end
+
+  #is_alive variable
+  def is_alive
+    @lives > 0
+  end
+
+end 
